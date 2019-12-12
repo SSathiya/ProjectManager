ae0969bc8e70131475c264120659f6cb9ad95236
moorts1 <moorts1@cat3093614.ap.cat.com>
2019-12-12 19:35:01 +0530

    Cognizant IIHT final Assesment

---
+++ ProjectManager/src/main/java/com/projectmanager/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.projectmanager.dao;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.projectmanager.collection.User;
+
+/**
+ * 
+ * @author Sathiya
+ *
+ */
+public interface UserDao extends MongoRepository<User, String> {
+		
+
+}
